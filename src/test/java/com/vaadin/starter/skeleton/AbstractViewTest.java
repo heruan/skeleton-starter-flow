8109ecb028a851852e79a9042757695233adeb3d
Joni Uitto <joni@vaadin.com>
2019-05-22 11:04:19 +0300

    Added some class comment

--- src/test/java/com/vaadin/starter/skeleton/AbstractViewTest.java
+++ src/test/java/com/vaadin/starter/skeleton/AbstractViewTest.java
@@ -5,21 +5,38 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.vaadin.testbench.ScreenshotOnFailureRule;
 import com.vaadin.testbench.TestBench;
 import com.vaadin.testbench.parallel.ParallelTest;
 
 /**
- * Base class for ITs.
+ * Base class for ITs
+ * <p>
+ * The tests use Chrome driver (see pom.xml for integration-tests profile) to
+ * run integration tests on a headless Chrome. If a property {@code test.use
+ * .hub} is set to true, {@code AbstractViewTest} will assume that the
+ * TestBench test is running in a CI environment. In order to keep the this
+ * class light, it makes certain assumptions about the CI environment (such
+ * as available environment variables). It is not advisable to use this class
+ * as a base class for you own TestBench tests.
+ * <p>
+ * To learn more about TestBench, visit
+ * <a href="https://vaadin.com/docs/v10/testbench/testbench-overview.html">Vaadin TestBench</a>.
  */
 public abstract class AbstractViewTest extends ParallelTest {
     private static final int SERVER_PORT = 8080;
 
     private final String route;
     private final By rootSelector;
+
+    @Rule
+    public ScreenshotOnFailureRule rule = new ScreenshotOnFailureRule(this,
+            true);
 
     public AbstractViewTest() {
         this("", By.tagName("body"));
@@ -90,7 +107,9 @@ public abstract class AbstractViewTest extends ParallelTest {
     private static final String USE_HUB_PROPERTY = "test.use.hub";
 
     /**
-     * Returns whether we are using a test hub
+     * Returns whether we are using a test hub. This means that the starter
+     * is running tests in Vaadin's CI environment, and uses TestBench to
+     * connect to the testing hub.
      *
      * @return whether we are using a test hub
      */
